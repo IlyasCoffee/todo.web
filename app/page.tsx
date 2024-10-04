7c52dde258b36e06ba3b531ab16d5ae4dd2e4280
Ilyas Husain <178883040+IlyasCoffee@users.noreply.github.com>
2024-10-04 18:31:34 +0300

    Add some new layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import {Sun, Clock, Star, CheckCircle2} from "lucide-react";
+import {Sun, Clock, Star, CheckCircle2, Search, Plus} from "lucide-react";
 
 export default function Home() {
   return (
@@ -17,7 +17,17 @@ export default function Home() {
           <div></div>
         </div>
 
-        <div className="h-full w-3/4"></div>
+        <div className="flex flex-col justify-start justify-items-start items-center h-full w-3/4">
+          <div className="border-b flex flex-col justify-center justify-items-center items-start w-full p-6">
+            <h1>TODAY</h1>
+            <h3>2024/10/4 - 1 Rabi II 1446</h3>
+          </div>
+
+          <div className="overflow-y-scroll overflow-x-hidden row-span-8 flex flex-col justify-start justify-items-start items-center w-full gap-4 p-6">
+          </div>
+
+          <button className="btn-primary absolute right-0 bottom-0 gap-3 m-9 p-3"><Plus/></button>
+        </div>
       </div>
     </main>
   );
